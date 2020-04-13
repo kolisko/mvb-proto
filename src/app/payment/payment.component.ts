08a848d356e5d47e80f7db1009f723f699bfdd38
Michal Kolesar <michal@zeleny-ctverec.cz>
2020-04-13 19:04:47 +0200

    payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AppService} from '../app.service';
+import {Configuration} from '../../config-loader';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-payment',
@@ -10,13 +12,28 @@ import {AppService} from '../app.service';
 export class PaymentComponent implements OnInit {
 
   constructor(private router: Router,
-              public appService: AppService) { }
+              public appService: AppService,
+              private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   onSendClick(): void {
-    this.router.navigate(['home']);
+    const paymentUrl: string = Configuration.get('paymentUrl');
+    paymentUrl.replace('{clientNumber}', this.appService.clientId);
+    const params = new HttpParams()
+      .append('amount', String(this.appService.piAmount))
+      .append('toAccount', this.appService.piAccount);
+    this.httpClient.post(paymentUrl, null, { params}).subscribe((next: any) => {
+      if (next.paymentState !== 'ACCEPTED') {
+        this.appService.alertMsg = 'Payment denied';
+        return;
+      }
+
+      this.router.navigate(['home']);
+    }, error => {
+      this.appService.alertMsg = 'AccountOverview Error';
+    });
   }
 
   onCancelClick(): void {
