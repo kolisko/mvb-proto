96cf3792f64f9fde4608961e63e81584fbdc6d62
Michal Kolesar <michal@zeleny-ctverec.cz>
2020-04-27 13:32:00 +0200

    fix mock data balance and transactions

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,20 +12,24 @@ export class AppService {
   email: string;
   clientId: string;
   accountNumber: string = '12345678/0100';
-  balance: number = 1000;
+  balance: number = 20600;
   transactions: Array<Transaction> = [
-    {fromAccount: '12345678/0100',
-    toAccount: '87654321/0100',
-    amount: -9000,
-    issueDate: '2020-03-03'},
+    {fromAccount: '222111222/5500',
+      toAccount: '12345678/0100',
+      amount: 20000,
+      issueDate: '2020-04-04'},
     {fromAccount: '12345678/0100',
       toAccount: '87654321/0100',
       amount: -400,
       issueDate: '2020-04-03'},
-    {fromAccount: '222111222/5500',
-      toAccount: '12345678/0100',
-      amount: 20000,
-      issueDate: '2020-04-04'}
+    {fromAccount: '12345678/0100',
+    toAccount: '87654321/0100',
+    amount: -9000,
+    issueDate: '2020-03-03'},
+    {fromAccount: '111333555/0800',
+    toAccount: '12345678/0100',
+    amount: 10000,
+    issueDate: '2020-03-03'}
   ];
 
   piAccount: string;
